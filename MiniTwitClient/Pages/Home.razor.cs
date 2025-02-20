0911b3b79a90359c8b6139d506489a184436b418
Lukski175 <113596146+Lukski175@users.noreply.github.com>
2025-02-20 14:42:57 +0100

    tested tailwind

--- MiniTwitClient/Pages/Home.razor.cs
+++ MiniTwitClient/Pages/Home.razor.cs
@@ -4,9 +4,11 @@ namespace MiniTwitClient.Pages
 {
     public partial class Home : ComponentBase
     {
+        private string InputString { get; set; }
+
         private void ButtonClick()
         {
-            Console.WriteLine("You clicked the button!");
+            Console.WriteLine($"You clicked the button: {InputString}");
         }
     }
 }
