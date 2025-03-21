c04d1e63914059bd622c8f68e6a1231ecec52df4
lukas175 <lukas.a.rasmussen@gmail.com>
2025-03-21 11:55:24 +0100

    Update RegisterRequest.cs

--- MiniTwitAPI/Models/RegisterRequest.cs
+++ MiniTwitAPI/Models/RegisterRequest.cs
@@ -1,14 +1,11 @@
-﻿namespace MiniTwitAPI.Models
+﻿using System.Text.Json.Serialization;
+
+namespace MiniTwitAPI.Models
 {
     public class RegisterRequest
     {
         public string Username { get; set; }
         public string Email { get; set; }
-<<<<<<< Updated upstream
-        public string Password { get; set; }
-        public string Password2 { get; set; }
-=======
         [JsonPropertyName("pwd")] public string Password { get; set; }
->>>>>>> Stashed changes
     }
 }
